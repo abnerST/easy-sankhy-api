aef7760dd801d5d29a487cd0e57d5eff8272839c
AbnerST <abnersouza287@outlook.com>
2024-10-04 15:11:29 -0300

    Commit inicial

---
+++ src/main/java/com/zab/easy_sankhya_api/EasySankhyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.zab.easy_sankhya_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySankhyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySankhyaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zab/easy_sankhya_api/EasySankhyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zab.easy_sankhya_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySankhyaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
