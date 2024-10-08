cd8e376155647c24e74b18db4b471a47f725f6d6
AbnerST <abnersouza287@outlook.com>
2024-10-08 16:07:37 -0300

    Simple ident

--- src/main/java/com/zab/easy_sankhya_api/EasySankhyaApiApplication.java
+++ src/main/java/com/zab/easy_sankhya_api/EasySankhyaApiApplication.java
@@ -7,10 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.zab.easy_sankhya_api.client")  // Pacote onde seu LoginClient está definido
 public class EasySankhyaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EasySankhyaApiApplication.class, args);
-
 	}
-
 }
